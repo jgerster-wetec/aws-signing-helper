91edd8479e30e650f5b53d68517ee79fa6b3f1cd
jgerster-wetec <91420980+jgerster-wetec@users.noreply.github.com>
2024-12-12 14:10:05 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,6 +26,6 @@ then
   eEBUG=false
 fi
 aws_signing_helper serve --port $PORT --certificate $CERTIFICATE --private-key $PRIVATE_KEY --trust-anchor-arn $TRUST_ANCHOR_ARN --profile-arn $PROFILE_ARN --role-arn $ROLE_ARN --endpoint $ENDPOINT --region $REGION --debug $DEBUG &
-echo "socat listens on 'aws-signing-helper:9911 and forwards requests to localhost:9911 where aws-signing-helper is listening"
-echo "export AWS_EC2_METADATA_SERVICE_ENDPOINT=http://aws-signing-helper:9911/ in docker containers to make use of credentials provided via aws-signing-helper service"
+echo "socat listens on 'aws-signing-helper:$PORT' and forwards requests to 'localhost:$PORT', where aws-signing-helper is listening"
+echo "export AWS_EC2_METADATA_SERVICE_ENDPOINT=http://aws-signing-helper:$PORT/ in docker containers to make use of credentials provided via aws-signing-helper service"
 socat tcp4-listen:$PORT,bind=aws-signing-helper,reuseaddr,fork tcp4:127.0.0.1:$PORT
